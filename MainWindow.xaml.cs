0380e2be997b3e851b55030c9efd0e40a7c7eb9b
Campbell <KidzCode42@gmail.com>
2021-03-20 12:23:00 -0600

    We fixed a bug that didn't execute the 2x2 and 3x3 rules properly

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -390,7 +390,7 @@ namespace Sudoku
 					if (sudokuSolver.Solve(GetColumn(c)) == SolveResult.SquaresSolved)
 						squaresSolved = true;
 				for (int r = 0; r < 9; r++)
-					if (sudokuSolver.Solve(GetColumn(r)) == SolveResult.SquaresSolved)
+					if (sudokuSolver.Solve(GetRow(r)) == SolveResult.SquaresSolved)
 						squaresSolved = true;
 				for (int r = 0; r < 3; r++)
 					for (int c = 0; c < 3; c++)
