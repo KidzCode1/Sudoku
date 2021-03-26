080a2358f890fb2774292ba4d8751f3a661f6ae0
Campbell <KidzCode42@gmail.com>
2021-03-26 16:11:30 -0600

    Refactored to support NxN solving rules.

--- Solver3x3.cs
+++ Solver3x3.cs
@@ -4,74 +4,35 @@ using System.Linq;
 
 namespace Sudoku
 {
-	public class Solver3x3 : BaseGroupSolver
+	public class Solver3x3: SolverNxN
 	{
-
+		
 		public Solver3x3()
 		{
-
+			
 		}
 
 		public override SolveResult Solve(ISudokuSquare[] group)
 		{
 			SolveResult result = SolveResult.None;
-			
+
 			for (int first = 0; first < group.Length; first++)
 				for (int second = 0; second < group.Length; second++)
 					if (second == first)
 						continue;
 					else
-					{
 						for (int third = 0; third < group.Length; third++)
 							if (third == first || third == second)
 								continue;
 							else
 							{
-								// Magic! I've got three squares!
-								ISudokuSquare firstSquare = group[first];
-								List<int> firstNoteNumbers = GetNumbers(firstSquare.Notes);
-								if (firstNoteNumbers.Count < 2 || firstNoteNumbers.Count > 3)
-									continue;
-
-								ISudokuSquare secondSquare = group[second];
-								List<int> secondNoteNumbers = GetNumbers(secondSquare.Notes);
-								if (secondNoteNumbers.Count < 2 || secondNoteNumbers.Count > 3)
-									continue;
-
-								ISudokuSquare thirdSquare = group[third];
-								List<int> thirdNoteNumbers = GetNumbers(thirdSquare.Notes);
-								if (thirdNoteNumbers.Count < 2 || thirdNoteNumbers.Count > 3)
-									continue;
-
-								// Super magic!!!
-								// We have three squares that all contain 2 or 3 notes!!!
-								List<int> allNumbersFound = new List<int>();
-								AddNumbersTo(allNumbersFound, firstNoteNumbers);
-								AddNumbersTo(allNumbersFound, secondNoteNumbers);
-								AddNumbersTo(allNumbersFound, thirdNoteNumbers);
-								if (allNumbersFound.Count > 3)
-									continue;
-								else
-								{
-									for (int i = 0; i < group.Length; i++)
-									{
-										if (i == first || i == second || i == third)
-											continue;
-										// Now we can actually remove!!!
-										if (RemoveNotesFrom(group[i], string.Join(", ", allNumbersFound)) == SolveResult.SquaresSolved)
-											result = SolveResult.SquaresSolved;
-									}
-								}
+								List<int> indices = new List<int>();
+								indices.Add(first);
+								indices.Add(second);
+								indices.Add(third);
+								result = SolveForMany(group, result, indices);
 							}
-					}
 			return result;
-		}
-
-		private static void AddNumbersTo(List<int> allNumbersFound, List<int> firstNoteNumbers)
-		{
-			foreach (int number in firstNoteNumbers)
-				if (!allNumbersFound.Contains(number))
-					allNumbersFound.Add(number);
 		}
 	}
 }
---
+++ SolverNxN.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sudoku
+{
+	public abstract class SolverNxN : BaseGroupSolver
+	{
+
+		public SolverNxN()
+		{
+
+		}
+
+
+		protected SolveResult SolveForMany(ISudokuSquare[] group, SolveResult result, List<int> indices)
+		{
+			List<List<int>> allNoteNumbers = new List<List<int>>();
+			foreach (int index in indices)
+			{
+				ISudokuSquare square = group[index];
+				List<int> squareNoteNumbers = GetNumbers(square.Notes);
+				allNoteNumbers.Add(squareNoteNumbers);
+				if (squareNoteNumbers.Count < 2 || squareNoteNumbers.Count > indices.Count)
+					return SolveResult.None;
+			}
+
+			// Super magic!!!
+			// We have N squares that all contain N notes or fewer!!!
+			List<int> allNumbersFound = new List<int>();
+			foreach (List<int> noteNumbers in allNoteNumbers)
+				AddNumbersTo(allNumbersFound, noteNumbers);
+
+			if (allNumbersFound.Count > indices.Count)
+				return SolveResult.None;
+			else
+				for (int i = 0; i < group.Length; i++)
+				{
+					if (indices.Contains(i))
+						continue;
+					// Now we can actually remove!!!
+					if (RemoveNotesFrom(group[i], string.Join(", ", allNumbersFound)) == SolveResult.SquaresSolved)
+						result = SolveResult.SquaresSolved;
+				}
+
+			return result;
+		}
+
+		private static void AddNumbersTo(List<int> targetNumbers, List<int> sourceNumbers)
+		{
+			foreach (int number in sourceNumbers)
+				if (!targetNumbers.Contains(number))
+					targetNumbers.Add(number);
+		}
+	}
+}
